b4b1a95490ddf4c79d73731696e53565fc833803
xiugaze <calebandreano@gmail.com>
2022-07-30 21:07:06 -0500

    fixed result type again

--- src/core/auth/strings.rs
+++ src/core/auth/strings.rs
@@ -1,7 +1,8 @@
+// TODO:: Every function should return a Result for custom error type
 use bech32::{self, FromBase32, ToBase32, Variant, u5};
-use bech32::Error as b32Err;
+// use bech32::Error as b32Err;
 use bech32::Error::*;
-use reqwest::Error as rqErr;
+// use reqwest::Error as rqErr;
 use core::result::Result as Result;
 
 fn check_prefix_and_length(prefix: &str, data: &str, length: i32) -> bool {
@@ -26,19 +27,12 @@ fn is_acc_address(data: &str) -> bool {
     check_prefix_and_length("secret", data, 45)
 }
 
-fn to_acc_address(data: ValAddress) -> Result<AccAddress, b32Err> {
-    let vals = bech32::decode(&data.val);
-    match vals {
-        Ok(t) => {
-            match bech32::encode("secret", t.1, t.2) {
-                Ok(string) => Ok(AccAddress {
-                    val: string,
-                }),
-                Err(e) => Err(e),
-            }
-        },
-        Err(e) => Err(e),
-    }
+fn to_acc_address(data: ValAddress) -> Result<AccAddress, bech32::Error> {
+    let decode = bech32::decode(&data.val)?;
+    let encode = bech32::encode("secret", decode.1, decode.2)?;
+    Ok(AccAddress {
+        val: encode,
+    })
 }
 
 
@@ -51,20 +45,13 @@ fn is_val_address(data: &str) -> bool {
     return check_prefix_and_length("secretvaloper", data, 52);
 }
 
-fn to_val_address(data: AccAddress) -> Result<ValAddress, b32Err> {
-    let vals = bech32::decode(&data.val);
-    match vals {
-        Ok(t) => {
-            match bech32::encode("secretvaloper", t.1, t.2) {
-                Ok(string) => Ok(ValAddress {
-                    val: string,
-                }),
-                Err(e) => Err(e),
-            }
-        },
-        Err(e) => Err(e),
-    }
 
+fn to_val_address(data: AccAddress) -> Result<ValAddress, bech32::Error> {
+    let decode = bech32::decode(&data.val)?;
+    let encode = bech32::encode("secretvaloper", decode.1, decode.2)?;
+    Ok(ValAddress {
+        val: encode,
+    })
 }
 
 // // **********  AccPubKey *********** //
@@ -75,21 +62,13 @@ fn is_acc_pubkey(data: &str) -> bool {
     return check_prefix_and_length("secretpub", data, 77);
 }
 
-fn to_acc_pubkey(data: ValPubKey) -> Result<AccPubKey, b32Err> {
-    let vals = bech32::decode(&data.val);
-    match vals {
-        Ok(t) => {
-            match bech32::encode("secretpub", t.1, t.2) {
-                Ok(string) => Ok(AccPubKey {
-                    val: string,
-                }),
-                Err(e) => Err(e),
-            }
-        },
-        Err(e) => Err(e),
-    }
+fn to_acc_pubkey(data: ValPubKey) -> Result<AccPubKey, bech32::Error> {
+    let decode = bech32::decode(&data.val)?;
+    let encode = bech32::encode("secretpub", decode.1, decode.2)?;
+    Ok(AccPubKey {
+        val: encode,
+    })
 }
-
 
 // // **********  ValPubKey *********** //
 pub struct ValPubKey {
@@ -100,21 +79,13 @@ fn is_val_pubkey(data: &str) -> bool {
     return check_prefix_and_length("secretvaloperpub", data, 84);
 }
 
-fn to_val_pubkey(data: AccPubKey) -> Result<ValPubKey, b32Err> {
-    let vals = bech32::decode(&data.val);
-    match vals {
-        Ok(t) => {
-            match bech32::encode("secretvaloperpub", t.1, t.2) {
-                Ok(string) => Ok(ValPubKey {
-                    val: string,
-                }),
-                Err(e) => Err(e),
-            }
-        },
-        Err(e) => Err(e),
-    }
+fn to_val_pubkey(data: AccPubKey) -> Result<ValPubKey, bech32::Error> {
+    let decode = bech32::decode(&data.val)?;
+    let encode = bech32::encode("secretvaloperpub", decode.1, decode.2)?;
+    Ok(ValPubKey {
+        val: encode,
+    })
 }
-
 
 
 // // **********  ValConsPubKey *********** //
@@ -126,11 +97,3 @@ fn is_valcons_pubkey(data: &str) -> bool {
     return check_prefix_and_length("secretvalconspub", data, 83)
 }
 
-
-fn to_reqwest_error(err: b32Err) -> rqErr {
-    match err {
-        InvalidChar(c) => {},
-        InvalidData(num) => {},
-        others => {},
-    }
-} 
