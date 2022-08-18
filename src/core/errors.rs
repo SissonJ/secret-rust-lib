480fbfc815bbb1e7f95889c243feea1282303222
xiugaze <calebandreano@gmail.com>
2022-08-18 18:26:01 -0500

    progress

--- src/core/errors.rs
+++ src/core/errors.rs
@@ -1,4 +1,21 @@
-pub enum SecretError {
-    Bech32Error(String),
-    Error(String),
+use std::fmt;
+
+type Result<T> = std::result::Result<T, Error>; 
+
+#[derive(Clone, PartialEq, Debug)]
+pub enum Error {
+    NumberError(i32),
+    DecNumError(i128),
+    DecStringError(String),
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::NumberError(i) => write!(f, "NumberError for i32 {}", i),
+            Error::DecNumError(i) => write!(f, "DecNumError for Dec{{ i: {} }}", i),
+            Error::DecStringError(m) => write!(f, "DecNumError: {}", m),
+        }
+    }
+}
+
